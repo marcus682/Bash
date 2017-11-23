060b40f23f83886b581899df3f7590941614d174
Simon B <sibe39@users.noreply.github.com>
2017-11-23 14:19:08 +0100

    Update linuxPI.sh

--- linuxPI.sh
+++ linuxPI.sh
@@ -60,23 +60,54 @@ fi
 ##################################
 if [ "$choixProfil" = "2" ] || [ "$choixProfil" = "6" ] || [ "$choixProfil" = "11" ]
 then
+  # activation des dépots utiles
+  wget https://raw.githubusercontent.com/sibe39/linux_distrib/master/debian9-sources.list 
+  mv -f debian9-sources.list /etc/apt/sources.list && dpkg --add-architecture i386 && apt update
+  
+  # nettoyage puis maj du système
+  apt autoremove --purge -y && apt clean
+  apt update && apt full-upgrade -y
 
-
-
-
+  # outils utiles
+  apt install curl net-tools git openjdk-8-jre numlockx flatpak screenfetch -y
+  # éditeur/bureautique
+  apt install vim libreoffice libreoffice-l10n-fr libreoffice-help-fr evince -y
+  echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | /usr/bin/debconf-set-selections | apt install ttf-mscorefonts-installer -y
+  # codecs
+  apt install x264 x265 -y
+  # internet
+  apt install firefox-esr firefox-esr-l10n-fr chromium chromium-l10n pidgin transmission-gtk -y
+  # multimedia & graphisme
+  apt install vlc gimp gimp-help-fr shutter -y
+  # thème
+  apt install arc-thene numix-gtk-theme numix-icon-theme breeze-icon-theme breeze-cursor-theme -y
+  # optimisation swap
+  echo vm.swappiness=5 | tee /etc/sysctl.d/99-swappiness.conf   
 fi
-
-
 
 ##################################
 # Commun à tous - base Arch
 ##################################
 if [ "$choixProfil" = "3" ] || [ "$choixProfil" = "12" ]
-then
-
-
-
-
+then  
+  # nettoyage puis maj du système
+  pacman --noconfirm -Sc
+  pacman --noconfirm -Syyu
+  
+  # outils utiles
+  pacman --noconfirm -S curl net-tools git jre8-openjdk numlockx flatpak screenfetch 
+  # éditeur/bureautique
+  pacman --noconfirm -S vim libreoffice-fresh libreoffice-fresh-fr evince
+  # codecs
+  pacman --noconfirm -S x264 x265
+  # internet
+  pacman --noconfirm -S firefox firefox-i18n-fr chromium pidgin transmission-gtk
+  # multimedia & graphisme
+  pacman --noconfirm -S vlc gimp gimp-help-fr
+  # thème
+  pacman --noconfirm -S numix-gtk-theme breeze-gtk breeze-icons
+  # optimisation swap
+  echo vm.swappiness=5 | tee /etc/sysctl.d/99-swappiness.conf     
 fi
 
 ##################################
